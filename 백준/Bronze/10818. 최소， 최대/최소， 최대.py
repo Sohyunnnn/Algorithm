6c0a692447475c0678fb4ebda88a797381040895
Sohyunnnn <108279922+Sohyunnnn@users.noreply.github.com>
2024-05-20 18:24:38 +0900

    Update 최소， 최대.py

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,3 +1,15 @@
-cnt = int(input())
-numbers = list(map(int, input().split()))
-print(min(numbers),max(numbers))
+N= int(input())
+
+List= list(map(int, input().split()))
+
+Max= List[0]
+
+Min= List[0]
+
+for i in range(0,N):
+  if Max <= List[i]:
+    Max=List[i]
+  if Min >= List[i]:
+    Min= List[i]
+    
+print(Min, Max)
